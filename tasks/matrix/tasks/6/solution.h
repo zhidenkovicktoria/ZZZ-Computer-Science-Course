6f7d827b89bd578839de36267ce987453b225c1c
zhidenkovicktoria <144264972+zhidenkovicktoria@users.noreply.github.com>
2024-01-06 23:35:00 +0300

    Update solution.h

--- tasks/matrix/tasks/6/solution.h
+++ tasks/matrix/tasks/6/solution.h
@@ -12,61 +12,61 @@ int Task() {
     int count = 1;
     int32_t* massive = (int32_t*)malloc(sizeof(int32_t) * 64);
     int k = 0;
-    while(count > 0) {
+    while (count > 0) {
         count = fscanf(file, "%d", &massive[k]);
         ++k;
     }
     int len;
     len = (int)sqrt(k);
     Matrix matrix = (Matrix)malloc(sizeof(int32_t*) * len);
-    for(int i = 0; i < len; ++i) {
+    for (int i = 0; i < len; ++i) {
         matrix[i] = (int32_t*)malloc(sizeof(int32_t) * len);
     }
     k = 0;
-    for(int i = 0; i < len; ++i) {
-        for(int j = 0; j < len; ++j) {
+    for (int i = 0; i < len; ++i) {
+        for (int j = 0; j < len; ++j) {
             matrix[i][j] = massive[k];
             ++k;
         }
     }
-    for(int i = 0; i < len; ++i) {
-        for(int j = 0; j < len; ++j) {
+    for (int i = 0; i < len; ++i) {
+        for (int j = 0; j < len; ++j) {
             printf("%d ", matrix[i][j]);
         }
         printf("\n");
     }
     int32_t Min = matrix[0][0];
     int32_t numbers[len];
-    for(int i = 0; i < len; ++i) {
+    for (int i = 0; i < len; ++i) {
         numbers[i] = 0;
     }
     int last;
-    for(int j = 0; j < len; ++j) {
-        for(int i = 0; i < len; ++i) {
-            if(matrix[i][j] < Min) {
+    for (int j = 0; j < len; ++j) {
+        for (int i = 0; i < len; ++i) {
+            if (matrix[i][j] < Min) {
                 Min = matrix[i][j];
-                for(int h = 0; h < j; ++h) {
+                for (int h = 0; h < j; ++h) {
                     numbers[h] = 0;
                 }
                 numbers[j] = 1;
                 last = j;
-            } else if(matrix[i][j] == Min) {
+            } else if (matrix[i][j] == Min) {
                 numbers[j] = 1;
                 last = j;
             }
         }
     }
-    for(int j = 0; j < len; ++j) {
-        if(numbers[j] == 1) {
-            for(int i = 0; i < len; ++i) {
-                if(j != last) {
+    for (int j = 0; j < len; ++j) {
+        if (numbers[j] == 1) {
+            for (int i = 0; i < len; ++i) {
+                if (j != last) {
                     matrix[i][last] += matrix[i][j];
                 }
             }
         }
     }
     printf("\n");
-    for(int i = 0; i < len; ++i) {
+    for (int i = 0; i < len; ++i) {
         for(int j = 0; j < len; ++j) {
             printf("%d ", matrix[i][j]);
         }
@@ -74,7 +74,7 @@ int Task() {
     }
     fclose(file);
     free(massive);
-    for(int i = 0; i < len; ++i) {
+    for (int i = 0; i < len; ++i) {
         free(matrix[i]);
     }
     free(matrix);
