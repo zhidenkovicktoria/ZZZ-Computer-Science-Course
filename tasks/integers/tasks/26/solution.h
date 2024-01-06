edadd9623b5769485c1ee8a6f13f40587366c96d
zhidenkovicktoria <144264972+zhidenkovicktoria@users.noreply.github.com>
2024-01-06 22:49:09 +0300

    Update solution.h

--- tasks/integers/tasks/26/solution.h
+++ tasks/integers/tasks/26/solution.h
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-int64_t power(int64_t num1, int64_t num2) {
+int64_t Power(int64_t num1, int64_t num2) {
     int64_t res = 1;
     for (int64_t k = 1; k <= num2; k++) {
         res *= num1;
@@ -26,10 +26,10 @@ int Task() {
     int64_t number_2 = 0;
     int64_t result = 0;
     while (count >= 2) {
-        number_1 = number / power(10, count - 1);
-        number_2 = number / power(10, count - 2);
+        number_1 = number / Power(10, count - 1);
+        number_2 = number / Power(10, count - 2);
         number_2 %= 10;
-        number = number % (int64_t)power(10, count - 2);
+        number = number % (int64_t)Power(10, count - 2);
         count -= 2;
         result = result * 100 + 10 * number_1 + number_2;
         if (number_1 + number_2 < 10) {
