3e00a60055cce4fa7f3d3b9e54878efd004a5a56
zhidenkovicktoria <144264972+zhidenkovicktoria@users.noreply.github.com>
2024-01-06 22:21:06 +0300

    integers26_zhidenko

--- tasks/integers/tasks/26/solution.h
+++ tasks/integers/tasks/26/solution.h
@@ -3,7 +3,42 @@
 #include <stdint.h>
 #include <stdio.h>
 
+int64_t power(int64_t num1, int64_t num2) {
+    int64_t res = 1;
+    for (int64_t k = 1; k <= num2; k++) {
+        res *= num1;
+    }
+    return res;
+}
+
 int Task() {
     // write your solution here
+    int64_t number;
+    int64_t count = 1;
+    scanf("%d", &number);
+    int64_t number_save = number;
+    while (number / 10 != 0) {
+        number = number / 10;
+        count++;
+    }
+    number = number_save;
+    int64_t number_1;
+    int64_t number_2;
+    int64_t result = 0;
+    while (count >= 2) {
+        number_1 = number / power(10, count - 1);
+        number_2 = number / power(10, count - 2);
+        number_2 %= 10;
+        number = number % (int64_t)power(10, count - 2);
+        count -= 2;
+        result = result * 100 + 10 * number_1 + number_2;
+        if (number_1 + number_2 < 10){
+            result = result * 10 + number_1 + number_2;
+        }
+    }
+    if (number) {
+        result = result * 10 + number;
+    }
+    printf("%d\n", result);
     return 0;
 }
