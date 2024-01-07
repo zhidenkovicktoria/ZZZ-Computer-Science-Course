82db11f8f3547b220a1ccfa54904d117f7a9f291
zhidenkovicktoria <144264972+zhidenkovicktoria@users.noreply.github.com>
2024-01-07 18:59:19 +0300

    FSM_23_zhidenko

--- tasks/finite_state_machine/tasks/23/FSM.h
+++ tasks/finite_state_machine/tasks/23/FSM.h
@@ -1,14 +1,58 @@
 #pragma once
 
 #include <stdio.h>
+#include <stdint.h>
 
-typedef enum states {
-    S0,
-    S1
+typedef enum {
+    START,
+    OPEN_FILE,
+    READ_FILE,
+    CHECK,
+    PRINT,
+    CLOSE_FILE,
+    END
     // struct with states for your KA
 } State;
 
 int Task() {
     // write your solution here
+    State state = START;
+    char* path = "../tasks/finite_state_machine/tasks/23/file.txt";
+    int64_t LIM = 65535;
+    int64_t number = 0;
+    int64_t count = 0;
+    while (state != END) {
+        switch(state) {
+            case START:
+                state = OPEN_FILE;
+                break;
+            case OPEN_FILE:
+                FILE* file = fopen(path, "r");
+                state = READ_FILE;
+                break;
+            case READ_FILE:
+                if (feof(file) != 1){
+                    fscanf(file, "%ld", &number);
+                    state = CHECK;
+                    break;
+                }
+                state = PRINT;
+                break;
+            case CHECK:
+                if (number > 0 && number < LIM) {
+                    count++;
+                }
+                state = READ_FILE;
+                break;
+            case PRINT:
+                printf("%ld\n", count);
+                state = CLOSE_FILE;
+                break;
+            case CLOSE_FILE:
+                fclose(file);
+                state = END;
+                break;
+        }
+    }
     return 0;
 }
