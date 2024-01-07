1b79c8a12114d5e8bc32bd8e6f799a6ed940b4e8
zhidenkovicktoria <144264972+zhidenkovicktoria@users.noreply.github.com>
2024-01-07 11:32:31 +0300

    Update solution.h

--- tasks/bitsets/tasks/7/solution.h
+++ tasks/bitsets/tasks/7/solution.h
@@ -6,46 +6,46 @@
 typedef uint32_t Bitset;
 
 Bitset Consonants() {
-    Bitset Consonants = 0;
+    Bitset consonants = 0;
     for (unsigned char i = 'b'; i <= 'z'; ++i) {
         if (i != 'o' && i != 'e' && i != 'i' && i != 'y' && i != 'u') {
-            Consonants = Consonants | (1u << (i - 'a'));
+            consonants = consonants | (1u << (i - 'a'));
         }
     }
-    return Consonants;
+    return consonants;
 }
 
 int Task() {
     // write your solution here
-    Bitset Consonants_Set = Consonants();
+    Bitset consonants_Set = Consonants();
     int answer = 0;
     int flag = 0;
-    Bitset Set = 0;
-    unsigned char letter_1;
-    unsigned char letter_2;
+    Bitset set = 0;
+    unsigned char letter_1 = 0;
+    unsigned char letter_2 = 0;
     letter_1 = getchar();
     letter_2 = letter_1;
-    while (1) {
-        if ('a' <= letter_1 && letter_1 <= 'z' || 'A' <= letter_1 && letter_1 <= 'Z') {
+    while (true) {
+        if (('a' <= letter_1 && letter_1 <= 'z') || ('A' <= letter_1 && letter_1 <= 'Z')) {
             if (flag == 0) {
                 if ('a' <= letter_1 && letter_1 <= 'z') {
-                    Set = Set | (1u << (letter_1 - 'a'));
+                    set = set | (1u << (letter_1 - 'a'));
                 } else {
-                    Set = Set | (1u << (letter_1 - 'A'));
+                    set = set | (1u << (letter_1 - 'A'));
                 }
                 flag = 1;
             }
         } else if (flag) {
             if ('a' <= letter_2 && letter_2 <= 'z') {
-                Set = Set | (1u << (letter_2 - 'a'));
+                set = set | (1u << (letter_2 - 'a'));
             } else {
-                Set = Set | (1u << (letter_2 - 'A'));
+                set = set | (1u << (letter_2 - 'A'));
             }
             flag = 0;
-            if ((Set & Consonants_Set) == 0) {
+            if ((set & consonants_Set) == 0) {
                 answer = 1;
             }
-            Set = 0;
+            set = 0;
         }
         letter_2 = letter_1;
         if (letter_1 == '\n') {
