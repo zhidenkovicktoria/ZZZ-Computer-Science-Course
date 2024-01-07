721f32fc39d8a32ece69f08a0318a5c070c977d0
zhidenkovicktoria <144264972+zhidenkovicktoria@users.noreply.github.com>
2024-01-07 11:16:11 +0300

    Update solution.h

--- tasks/bitsets/tasks/7/solution.h
+++ tasks/bitsets/tasks/7/solution.h
@@ -8,7 +8,7 @@ typedef uint32_t Bitset;
 Bitset Consonants() {
     Bitset Consonants = 0;
     for (unsigned char i = 'b'; i <= 'z'; ++i){
-        if (i != 'o' && i != 'e' && i!= 'i' && i != 'y' && i != 'u'){
+        if (i != 'o' && i != 'e' && i!= 'i' && i != 'y' && i != 'u') {
             Consonants = Consonants | (1u << (i - 'a'));
         }
     }
@@ -25,37 +25,37 @@ int Task() {
     unsigned char letter_2;
     letter_1 = getchar();
     letter_2 = letter_1;
-    while (1){
-        if ('a' <= letter_1 && letter_1 <= 'z' || 'A' <= letter_1 && letter_1 <= 'Z'){
-            if (flag == 0){
-                if ('a' <= letter_1 && letter_1 <= 'z'){
+    while (1) {
+        if ('a' <= letter_1 && letter_1 <= 'z' || 'A' <= letter_1 && letter_1 <= 'Z') {
+            if (flag == 0) {
+                if ('a' <= letter_1 && letter_1 <= 'z') {
                     Set = Set | (1u << (letter_1 - 'a'));
-                } else{
+                } else {
                     Set = Set | (1u << (letter_1 - 'A'));
                 }
                 flag = 1;
             }
-        } else if (flag){
+        } else if (flag) {
             if ('a' <= letter_2 && letter_2 <= 'z'){
                     Set = Set | (1u << (letter_2 - 'a'));
-                } else{
+                } else {
                     Set = Set | (1u << (letter_2 - 'A'));
                 }
             flag = 0;
-            if ((Set & Consonants_Set) == 0){
+            if ((Set & Consonants_Set) == 0) {
                 answer = 1;
             }
             Set = 0;
         }
         letter_2 = letter_1;
-        if (letter_1 == '\n'){
+        if (letter_1 == '\n') {
             break;
         }
         letter_1 = getchar();
     }
-    if (answer){
+    if (answer) {
         printf("Есть слова, начинающиеся и заканчивающиеся гласными.\n");
-    } else{
+    } else {
         printf("Нет слов, начинающихся и заканчивающихся гласными.\n");
     }
     return 0;
